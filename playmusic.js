84deed27bdf1d2b301f79ad01407165e8e5ac5fe
dnlsrb <144025551+dnlsrb@users.noreply.github.com>
2026-02-14 22:20:09 +0800

    PART 4

--- playmusic.js
+++ playmusic.js
@@ -1,9 +1,9 @@
  const button = document.getElementById('playButton');
 const audio = document.getElementById('myAudio');
  const leaf = document.querySelector('.flower__leaf');
-const text2 = document.getElementById('Tx');
+ 
  button.addEventListener('click', () => {
-    text2.style.visibility = 'visible';
+ 
       audio.play();
       button.style.visibility = 'hidden';
       typeWriter();
