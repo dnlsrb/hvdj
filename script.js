0e4547bf7be308eb03150eefdead7a88ac4cd9da
dnlsrb <144025551+dnlsrb@users.noreply.github.com>
2026-03-05 20:29:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ if (!isMobile && !isSmallScreen) {
 
  
 var i = 0;
-var txt = "Happy Valentines day.. Jov! sana di tumigil yung song if tumigil maybe sa slow ng server haha, enjoy your day po and sa mga anakish nyo din dyan kila tita, tito. i hope masaya ang araw mo ngayon and sana you visited this site that i made specially for you,aaaaaaand thennnn mga 1 week ago, i really wanted to invite you to be my valentines date or simba, stranger date haha, But I think it's impossible so instead I made this site. Soooooo even though we don’t have chance na to see each other, i still wishes you for the happiest jov and for dream come true jov kahit di na kita makikita don sa part nayon buong buhay ko po iwiwish na you’ll be successful in life, i hope you achieve the things you wanted and the things that you deserve so yon lang. - Dan.\n ingat ka palagi imy <3";
+var txt = "Happy Valentines day.. Jov! sana di tumigil yung song if tumigil maybe sa slow ng server haha, enjoy your day po and sa mga anakish nyo din dyan kila tita, tito. i hope masaya ang araw mo ngayon and sana you visited this site that i made specially for you,aaaaaaand thennnn mga 1 week ago, i really wanted to invite you to be my valentines date or simba, stranger date haha, But I think it's impossible so instead I made this site. Soooooo even though we don’t have chance na to see each other, i still wishes you for the happiest jov and for dream come true jov kahit di na kita makikita don sa part nayon buong buhay ko po iwiwish na you’ll be successful in life, i hope you achieve the things you wanted and the things that you deserve so yon lang. - Dan.\n ingat ka palagi imy <3\n Thankyou for visiting this site";
 var speed = 100;
 
 function typeWriter() {
